67743166b9cbddb5373adf844ae644eecb999099
Salodki Dzmitry Piatrovich <dsalodki@gmail.com>
2021-08-09 14:06:33 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using UCO2.Models;
+
+namespace UCO2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View(new ViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewModel model)
+        {
+            var service = new PaymentService(new InputParams { CountPeople = model.PlayersNumber, InitAmount = model.StartAmount });
+            var result = service.Start().Result;
+            ViewBag.Result = result;
+            return View(new ViewModel());
+        }
+    }
+}
---
+++ Models/ViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UCO2.Models
+{
+    public class ViewModel
+    {
+        public decimal StartAmount { get; set; }
+        public int PlayersNumber { get; set; }
+    }
+
+    public class InputParams
+    {
+
+        public decimal InitAmount { get; set; }
+        public decimal BalanceAmount { get; set; }
+        public int CountPeople { get; set; }
+
+    }
+
+    public class PaymentService
+    {
+        private object _lockObj = new object();
+        private InputParams inputParams;
+
+        public PaymentService(InputParams inputParams)
+        {
+            this.inputParams = inputParams;
+        }
+
+        public async Task<InputParams> Start()
+        {
+            this.inputParams.BalanceAmount = this.inputParams.InitAmount;
+            List<Task> players = new List<Task>();
+            for (var i = 0; i < this.inputParams.CountPeople; i++)
+            {
+                var task = new Task(TakeAmount);
+                players.Add(task);
+            }
+            players.ForEach(x => x.Start());
+
+            Task.WaitAll(players.ToArray());
+            return this.inputParams;
+        }
+
+        private void TakeAmount()
+        {
+
+            while (true)
+            {
+                lock (_lockObj)
+                {
+                    if (CanTakeMore())
+                    {
+                        //Delay for data base transaction
+                        Task.Delay(100).Wait();
+                        this.inputParams.BalanceAmount = this.inputParams.BalanceAmount - 0.1M;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private bool CanTakeMore()
+        {
+            if (this.inputParams.BalanceAmount > 1.1M)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
